69d099227c924bb17f668222b23db763e4e3ca4a
Robson Perdigão <robson.perdigao@outlook.com>
2024-02-19 14:24:39 -0300

    corretagem

--- pages/opcoes.py
+++ pages/opcoes.py
@@ -116,14 +116,17 @@ def coleta_opcoes(ativo, vencimento):
     return df, df_put, df_call, preco_ativo
 
 # Operação - Collar de Alta
-def collar_alta(ativo, vencimento, quantidade = 1, volume_put = 0.01, negocios_put = 1, volume_call = 0.01, negocios_call = 1, filtro_data=None, risco=0):
+def collar_alta(ativo, vencimento, quantidade = 1, volume_put = 0.01, negocios_put = 1, volume_call = 0.01, 
+                negocios_call = 1, filtro_data=None, risco = 0.00, corretagem_variavel = 0.00, corretagem_ordem = 0.00):
     # Calcula CDI da operação
     cdi_operacao = calcula_cdi(vencimento)
     
     # Coleta as opções disponíveis para o ativo com base no vencimento determinado
     df, df_put, df_call, preco_ativo = coleta_opcoes(ativo, vencimento)
-       
-    df['custo'] = (df['preco_put'] - df['preco_call'] + preco_ativo) * quantidade
+    
+    tx_b3 = 0.1340
+    df['corretagem'] = ((preco_ativo + df['preco_put'] + df['preco_call']) * (corretagem_variavel + tx_b3)) + 6 * corretagem_ordem
+    df['custo'] = ((df['preco_put'] - df['preco_call'] + preco_ativo) * quantidade) - df['corretagem']
     df['cdi oper'] = cdi_operacao
     df['lucro minimo'] = df['strike_put'] * quantidade - df['custo']
     df['lucro min pct'] = round(df['lucro minimo'] / df['custo'] * 100, 2)
@@ -147,13 +150,16 @@ def collar_alta(ativo, vencimento, quantidade = 1, volume_put = 0.01, negocios_p
     return df, df_put, df_call, df_op
 
 # Operação - Collar de Baixa
-def collar_baixa(ativo, vencimento, quantidade = 1, volume_put = 0.01, negocios_put = 1, volume_call = 0.01, negocios_call = 1, filtro_data=None, risco=0):
+def collar_baixa(ativo, vencimento, quantidade = 1, volume_put = 0.01, negocios_put = 1, volume_call = 0.01, 
+                 negocios_call = 1, filtro_data=None, risco = 0.00, corretagem_variavel = 0.00, corretagem_ordem = 0.00):
     # Calcula CDI da operação
     cdi_operacao = calcula_cdi(vencimento)
     
     # Coleta as opções disponíveis para o ativo com base no vencimento determinado
     df, df_put, df_call, preco_ativo = coleta_opcoes(ativo, vencimento)
-       
+
+    tx_b3 = 0.1340
+    df['corretagem'] = ((preco_ativo + df['preco_put'] + df['preco_call']) * (corretagem_variavel + tx_b3)) + 6 * corretagem_ordem
     df['custo'] = (df['preco_put'] - df['preco_call'] + preco_ativo) * quantidade
     df['cdi oper'] = cdi_operacao
     df['lucro minimo'] = df['strike_call'] * quantidade - df['custo']
@@ -206,13 +212,18 @@ with st.container():
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         volume_put = st.slider('Volume mínimo da PUT', 0.01, 9999999.00, 500.00)
-        risco = st.number_input("Digite a % de risco aceitável pela operação", min_value=-100.00, value=calcula_cdi(vencimento), help='O valor padrão é o CDI até o vencimento da opção.\nAo definir o risco mínimo, será apresentado somente operações mais lucrativas (ou menos prejudiciais) que o valor escolhido.')
+        risco = st.number_input("% de risco aceitável pela operação", min_value=-100.00, value=calcula_cdi(vencimento), 
+                                help='O valor padrão é o CDI até o vencimento da opção.\nAo definir o risco mínimo, será apresentado somente operações mais lucrativas (ou menos prejudiciais) que o valor escolhido.')
     with col2:
-        negocios_put = st.slider('Quantidade mínima de negócios da PUT', 1, 1000, 1, 1)        
+        negocios_put = st.slider('Quantidade mínima de negócios da PUT', 1, 1000, 1, 1)
+        
+        st.write(f'Emolumentos B3: {tx_b3}% do volume')
     with col3:
         volume_call = st.slider('Volume mínimo da CALL', 0.01, 9999999.00, 500.00)
+        corretagem_variavel = st.number_input('Taxa de corretagem variável da sua corretora', min_value=0.00, max_value=5.00, value=0.50)
     with col4:
         negocios_call = st.slider('Quantidade mínima de negócios da CALL', 1, 1000, 1, 1)
+        corretagem_ordem = st.number_input('Taxa de corretagem por ordem', min_value=0.00, max_value=100.00, value=2.90)
     with col5:
         filtro_data = st.date_input('Data último negócio', format='DD/MM/YYYY')
 
@@ -232,10 +243,12 @@ with col3:
 with st.container():
     st.write('Lista de operações possíveis')
     if estrutura == 'Collar de Alta':
-        df, df_put, df_call, df_op = collar_alta(ativo, vencimento, quantidade, volume_put, negocios_put, volume_call, negocios_call, filtro_data, risco)
+        df, df_put, df_call, df_op = collar_alta(ativo, vencimento, quantidade, volume_put, negocios_put, volume_call, 
+                                                 negocios_call, filtro_data, risco, corretagem_variavel, corretagem_ordem)
         mostra_operacoes()
     elif estrutura == 'Collar de Baixa':
-        df, df_put, df_call, df_op = collar_baixa(ativo, vencimento, quantidade, volume_put, negocios_put, volume_call, negocios_call, filtro_data, risco)
+        df, df_put, df_call, df_op = collar_baixa(ativo, vencimento, quantidade, volume_put, negocios_put, volume_call, 
+                                                  negocios_call, filtro_data, risco, corretagem_variavel, corretagem_ordem)
         mostra_operacoes()
 
 st.markdown('---')
