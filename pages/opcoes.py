7944553c158de974895ea8a4e6e0b5e55c309339
Robson Perdigão <robson.perdigao@outlook.com>
2024-02-19 16:08:39 -0300

    corretagem

--- pages/opcoes.py
+++ pages/opcoes.py
@@ -122,7 +122,7 @@ def collar_alta(ativo, vencimento, quantidade = 1, volume_put = 0.01, negocios_p
     
     tx_b3 = 0.1340
     df['custo'] = (df['preco_put'] - df['preco_call'] + preco_ativo) * quantidade
-    df['corretagem'] = (df['custo'] * ((corretagem_variavel + tx_b3) / 100)) + 6 * corretagem_ordem
+    df['corretagem'] = round(((df['custo'] * ((corretagem_variavel + tx_b3) / 100)) + 6 * corretagem_ordem) * 100, 2)
     df['corretagem %'] = df['corretagem'] / df['custo']
     df['cdi oper'] = cdi_operacao
     df['lucro minimo'] = df['strike_put'] * quantidade - df['custo']
